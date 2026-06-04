6ac450cd96fcd95ed72381e0adbeb053f3438445
Developer <developer@example.com>
2026-06-04 11:37:04 +0530

    fix: add placeholder exports to CountryPage and Favourites components

--- src/pages/CountryPage.jsx
+++ src/pages/CountryPage.jsx
@@ -1,1 +1,8 @@
 // Placeholder - CountryPage will be implemented in Part 3
+
+function CountryPage() {
+  return <div>Country Page - Coming in Part 3</div>
+}
+
+export default CountryPage
+
--- src/pages/Favourites.jsx
+++ src/pages/Favourites.jsx
@@ -1,1 +1,8 @@
 // Placeholder - Favourites page will be implemented in Part 5
+
+function Favourites() {
+  return <div>Favourites Page - Coming in Part 5</div>
+}
+
+export default Favourites
+
